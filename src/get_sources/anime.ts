ab2cebb546b5dbf0ba5dbf0e42d8d700571d1af2
GoodDay360 <istartgame31@gmail.com>
2026-04-12 22:27:29 +0700

    fix query in get_sources

--- src/get_sources/anime.ts
+++ src/get_sources/anime.ts
@@ -4,7 +4,7 @@
 // Local Packages
 import type {InputPayload, OutputPayload} from "@plugin_provider/global/types/get_sources";
 import request from "@plugin_provider/method/request";
-
+import { IGNORE_TITLE } from ".";
 
 
 export default async function get(input_payload: InputPayload): Promise<OutputPayload> {
@@ -19,9 +19,10 @@ export default async function get(input_payload: InputPayload): Promise<OutputPa
     }).send();
 
     const data = res.body_json();
-
+    
     let output_payload = [];
     for (let i = 0; i < data.length; i++) {
+        if ((data[i].name??"" as string).toLowerCase().includes(IGNORE_TITLE)) continue;
         output_payload.push({
             id: data[i].id,
             title: data[i].name,

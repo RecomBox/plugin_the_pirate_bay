ca2b57e72a55aca12f96e0beb334cf26aa4e7cc9
GoodDay360 <istartgame31@gmail.com>
2026-04-13 22:30:42 +0700

    feat: add advanced default filter

--- src/get_sources/anime.ts
+++ src/get_sources/anime.ts
@@ -9,7 +9,16 @@ import { IGNORE_TITLE } from ".";
 
 export default async function get(input_payload: InputPayload): Promise<OutputPayload> {
 
-    let query_term = input_payload.search?.trim() ? input_payload.search.trim() : input_payload.title;
+    let prefer_title = input_payload.title_secondary ?? input_payload.title;
+
+    let season = input_payload.season.toString().padStart(2, "0");
+    let episode = input_payload.episode.toString().padStart(2, "0");
+
+    let formatted_query = `${prefer_title} S${season}E${episode}`.trim();
+
+    let query_term = input_payload.search?.trim() ? input_payload.search.trim() : formatted_query;
+
+    
 
     let url = `https://apibay.org/q.php?q=${encodeURIComponent(query_term)}&cat=0`
 
